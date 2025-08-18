d357fff7f3d088466daf4a3ffb2843ef3368e8f2
AnouarAyouch <anouarayouch01@gmail.com>
2025-08-18 17:43:36 +0100

    second commit

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-type ServeMux struct {
+import (
+	"fmt"
+	"net/http"
+)
+
+func start() {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println(err)
+	}
 }
 
 func main() {
+	start()
 }
+
