1d71abba66988e2349c7b397fcc860ca9bf97c35
AnouarAyouch <anouarayouch01@gmail.com>
2025-08-18 19:48:47 +0100

    jjj

--- main.go
+++ main.go
@@ -5,19 +5,26 @@ import (
 	"net/http"
 )
 
+func handleReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
 func FirstServer() {
+	const filepathRoot = "."
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handleReadiness)
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("./", http.FileServer(http.Dir("/assets")))
-
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Fatal(err)
 
 }
 
