4a1e2b070b4e212ca403072d518bcfa76ac7465c
AnouarAyouch <anouarayouch01@gmail.com>
2025-08-19 22:19:48 +0100

    the

--- main_2.go
+++ main_2.go
@@ -11,24 +11,38 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func StratingTheServer() {
+func main() {
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 	}
 
 	mux := http.NewServeMux()
-	Servport := "8080"
 	Serv := &http.Server{
-		Addr:    ":" + Servport,
+		Addr:    ":8080",
 		Handler: mux,
 	}
-	// Welcom to chairpy and image
+	// Welcom to chairpy and image handler
 	mux.Handle("/", http.FileServer(http.Dir(".")))
 	mux.Handle("/assets", http.FileServer(http.Dir(".")))
 	mux.HandleFunc("/ready", handlerReadiness)
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.Header().Set("Allow", http.MethodPost)   // tells client only POST is allowed
+			w.WriteHeader(http.StatusMethodNotAllowed) // sends 405
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(http.StatusText(http.StatusOK)))
+	})
+
+	// the hits
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	//mux.HandleFunc("POST /articles", handlerArticlesCreate)
 	err := Serv.ListenAndServe()
+
 	fmt.Println("Starting the server <<<>>>")
 	if err != nil {
 		log.Fatalf("can not start the server : %s", err)
@@ -36,9 +50,6 @@ func StratingTheServer() {
 }
 
 // the main
-func main() {
-	StratingTheServer()
-}
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -53,7 +64,25 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodPost)   // tells client only POST is allowed
+		w.WriteHeader(http.StatusMethodNotAllowed) // sends 405
+		return
+	}
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
 }
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)   // tells client only POST is allowed
+		w.WriteHeader(http.StatusMethodNotAllowed) // sends 405
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("hits restored to 0 "))
+}
+
+//func handlerArticlesCreate(w http.ResponseWriter, r *http.Request) {
+//
+//}
